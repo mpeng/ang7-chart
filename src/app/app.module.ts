5f56828956781ea0aaa586690900051d2935779c
mpeng <mpeng@bridge2solutions.com>
2018-12-08 20:05:35 -0500

    Add more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AdminComponent } from './Admin/admin.component';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { ComboChartComponent } from './Dashboard/Charts/combochart.component'
+import { DashboardComponent } from './Dashboard/dashboard.component';
+//import { DataTableModule } from 'angular-4-data-table';
+import { FormsModule }   from '@angular/forms';
+import { GoogleComboChartService } from './Services/google-combo-chart.service';
+import { GooglePieChartService } from './Services/google-pie-chart.service';
+import { NgModule } from '@angular/core';
+import { PieChartComponent } from './Dashboard/Charts/piechart.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AdminComponent,
+    ComboChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+//  DataTableModule ,
+    FormsModule
   ],
-  providers: [],
+  providers: [GoogleComboChartService,GooglePieChartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
